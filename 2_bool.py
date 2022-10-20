9cba83d0bbf8b02e365fa90866aecb6f7a445a53
Stanislav-Hryhorskyi <116120187+Stanislav-Hryhorskyi@users.noreply.github.com>
2022-10-20 20:24:00 +0300

    Add five new files on branch test

---
+++ 2_bool.py
@@ -0,0 +1,14 @@
+# Істинне значення
+boolean_1 = True
+
+# Хибне значение
+boolean_2 = False
+
+qwerty = 23.678
+print(bool(qwerty))
+
+print(boolean_1)
+print(boolean_2)
+
+class PressureInPipeline:
+    pass
